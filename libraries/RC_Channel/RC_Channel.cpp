5cd7d35c5ddd41eaaa33e55008eb456992e39413
Bill Geyer <bnsgeyer@aol.com>
2022-02-20 23:13:52 -0500

    RC_Channel: clean up style issues

--- libraries/RC_Channel/RC_Channel.cpp
+++ libraries/RC_Channel/RC_Channel.cpp
@@ -503,7 +503,7 @@ void RC_Channel::init_aux_function(const aux_func_t ch_option, const AuxSwitchPo
     case AUX_FUNC::SCRIPTING_7:
     case AUX_FUNC::SCRIPTING_8:
     case AUX_FUNC::VTX_POWER:
-	case AUX_FUNC::TURB_START:
+    case AUX_FUNC::TURB_START:
         break;
     case AUX_FUNC::AVOID_ADSB:
     case AUX_FUNC::AVOID_PROXIMITY:
