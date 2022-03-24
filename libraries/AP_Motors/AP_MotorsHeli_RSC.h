48dd7309389cf1371a32741a09f5f57e92822b58
Ferruccio1984 <niemals84@yahoo.it>
2022-03-24 08:39:41 -0700

    AP_Motors: Implementation of autorotation condition at RSC level
    enables faster re-spool(user settable timer) after power engagement

--- libraries/AP_Motors/AP_MotorsHeli_RSC.h
+++ libraries/AP_Motors/AP_MotorsHeli_RSC.h
@@ -130,6 +130,7 @@ public:
     AP_Int16        _critical_speed;          // Rotor speed below which flight is not possible
     AP_Int16        _idle_output;             // Rotor control output while at idle
     AP_Int16        _ext_gov_arot_pct;        // Percent value sent to external governor when in autorotation
+    AP_Int8         _rsc_bailout_time;        // time in seconds for power recovery
 
 private:
     uint64_t        _last_update_us;
@@ -161,6 +162,8 @@ private:
     float           _fast_idle_timer;             // cooldown timer variable
     uint8_t         _governor_fault_count;        // variable for tracking governor speed sensor faults
     float           _governor_torque_reference;   // governor reference for load calculations
+    bool            _autorotating;
+    bool            _bailing_out;
 
     // update_rotor_ramp - slews rotor output scalar between 0 and 1, outputs float scalar to _rotor_ramp_output
     void            update_rotor_ramp(float rotor_ramp_input, float dt);
