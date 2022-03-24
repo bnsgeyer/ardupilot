6a73fb3b74ead99188b008562e503f80307c4039
Ferruccio1984 <niemals84@yahoo.it>
2022-03-24 10:05:22 -0700

    Copter: Autorotation implementation
    -flare&touchdown controllers;
    -rangefinder for distance from ground evaluation;
    -higher refresh rate for rangefinder;
    -attitude targets implementation for "guided" autorotation;
    -Added touchdown timer for disarm after landing
    -rangefinder implementation, initial energy estimate for entry phase
    -fix yaw rate control during autorotation turns

--- ArduCopter/GCS_Mavlink.cpp
+++ ArduCopter/GCS_Mavlink.cpp
@@ -1118,7 +1118,7 @@ void GCS_MAVLINK_Copter::handleMessage(const mavlink_message_t &msg)
 
     switch (msg.msgid) {
 
-#if MODE_GUIDED_ENABLED == ENABLED
+#if MODE_GUIDED_ENABLED == ENABLED || MODE_AUTOROTATE_ENABLED == ENABLED
     case MAVLINK_MSG_ID_SET_ATTITUDE_TARGET:   // MAV ID: 82
     {
         // decode packet
@@ -1190,7 +1190,10 @@ void GCS_MAVLINK_Copter::handleMessage(const mavlink_message_t &msg)
 
         copter.mode_guided.set_angle(attitude_quat, ang_vel,
                 climb_rate_or_thrust, use_thrust);
-
+#if MODE_AUTOROTATE_ENABLED == ENABLED
+        copter.mode_autorotate.set_roll_angle(attitude_quat, ang_vel,
+                climb_rate_or_thrust, use_thrust);
+#endif
         break;
     }
 
--- ArduCopter/Parameters.cpp
+++ ArduCopter/Parameters.cpp
@@ -1246,7 +1246,7 @@ ParametersG2::ParametersG2(void)
     ,mode_systemid_ptr(&copter.mode_systemid)
 #endif
 #if MODE_AUTOROTATE_ENABLED == ENABLED
-    ,arot()
+    ,arot(copter.inertial_nav)
 #endif
 #if HAL_BUTTON_ENABLED
     ,button_ptr(&copter.button)
