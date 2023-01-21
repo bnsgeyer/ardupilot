5e884c5411dafbf334d2497f030619f124dea4e9
Bill Geyer <bnsgeyer@aol.com>
2023-01-20 23:27:53 -0500

    AP_MotorsHeli: don't let rotor_runup_output go below critical speed in autorotation

--- libraries/AP_Motors/AP_MotorsHeli_RSC.cpp
+++ libraries/AP_Motors/AP_MotorsHeli_RSC.cpp
@@ -434,7 +434,15 @@ void AP_MotorsHeli_RSC::update_rotor_runup(float dt)
             _rotor_runup_output = _rotor_ramp_output;
         }
     }else{
-        _rotor_runup_output = _rotor_ramp_output;
+        _rotor_runup_output -= runup_increment;
+        if (_rotor_runup_output < _rotor_ramp_output) {
+            _rotor_runup_output = _rotor_ramp_output;
+        }
+    }
+    // if in autorotation, don't let rotor_runup_output go less than critical speed to keep
+    // runup complete flag from being set to false
+    if (_autorotating && _rotor_runup_output < get_critical_speed()) {
+        _rotor_runup_output = get_critical_speed();
     }
 
     // update run-up complete flag
@@ -451,7 +459,7 @@ void AP_MotorsHeli_RSC::update_rotor_runup(float dt)
     }
     // if rotor speed is less than critical speed, then run-up is not complete
     // this will prevent the case where the target rotor speed is less than critical speed
-    if (_runup_complete && (get_rotor_speed() <= get_critical_speed())) {
+    if (_runup_complete && (get_rotor_speed() < get_critical_speed())) {
         _runup_complete = false;
     }
     // if rotor estimated speed is zero, then spooldown has been completed
