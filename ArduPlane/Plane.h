24706ac0bfa739ef8c6301c91d5f3c5a4d99148c
Andrew Tridgell <andrew@tridgell.net>
2025-02-06 17:59:09 +1100

    Plane: add system ID to quadplane
    enable as an AUX switch

--- ArduPlane/Plane.h
+++ ArduPlane/Plane.h
@@ -122,6 +122,7 @@
 #include "avoidance_adsb.h"
 #endif
 #include "AP_Arming.h"
+#include "systemid.h"
 
 /*
   main APM:Plane class
@@ -174,6 +175,9 @@ public:
 
 #if AP_EXTERNAL_CONTROL_ENABLED
     friend class AP_ExternalControl_Plane;
+#endif
+#if AP_PLANE_SYSTEMID_ENABLED
+    friend class AP_SystemID;
 #endif
 
     Plane(void);
@@ -901,6 +905,8 @@ private:
 
     void Log_Write_FullRate(void);
     void Log_Write_Attitude(void);
+    void Log_Write_SysID_Setup(uint8_t systemID_axis, float waveform_magnitude, float frequency_start, float frequency_stop, float time_fade_in, float time_const_freq, float time_record, float time_fade_out);
+    void Log_Write_SysID_Data(float waveform_time, float waveform_sample, float waveform_freq, float angle_x, float angle_y, float angle_z, float accel_x, float accel_y, float accel_z);
     void Log_Write_Control_Tuning();
     void Log_Write_OFG_Guided();
     void Log_Write_Guided(void);
